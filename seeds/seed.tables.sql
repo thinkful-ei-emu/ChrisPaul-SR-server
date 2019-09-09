4d055df04d358b697c58ba3fa51ecb6e45b61294
Chris Martin <chris.bizunit34@gmail.com>
2019-09-09 10:33:17 -0400

    updated to new langauge and updated tests

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,14 +21,14 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'practica', 'practice', 2),
+  (2, 1, 'hola', 'hello', 3),
+  (3, 1, 'casa', 'house', 4),
+  (4, 1, 'desarrollador', 'developer', 5),
+  (5, 1, 'traducir', 'translate', 6),
+  (6, 1, 'increible', 'amazing', 7),
+  (7, 1, 'perro', 'dog', 8),
+  (8, 1, 'gato', 'cat', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
--- src/user/user-service.js
+++ src/user/user-service.js
@@ -46,7 +46,7 @@ const UserService = {
       const [languageId] = await trx
         .into('language')
         .insert([
-          { name: 'French', user_id },
+          { name: 'Spanish', user_id },
         ], ['id'])
 
       // when inserting words,
@@ -58,14 +58,14 @@ const UserService = {
         .first()
 
       const languageWords = [
-        ['entraine toi', 'practice', 2],
-        ['bonjour', 'hello', 3],
-        ['maison', 'house', 4],
-        ['développeur', 'developer', 5],
-        ['traduire', 'translate', 6],
-        ['incroyable', 'amazing', 7],
-        ['chien', 'dog', 8],
-        ['chat', 'cat', null],
+        ['practica', 'practice', 2],
+        ['hola', 'hello', 3],
+        ['casa', 'house', 4],
+        ['desarrollador', 'developer', 5],
+        ['traducir', 'translate', 6],
+        ['increible', 'amazing', 7],
+        ['perro', 'dog', 8],
+        ['gato', 'cat', null],
       ]
 
       const [languageHeadId] = await trx
